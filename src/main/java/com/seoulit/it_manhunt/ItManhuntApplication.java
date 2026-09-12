0e6005858457e5ebafbcd18ba338829ef7803b89
wkdgurwn <zzoo8969@gmail.com>
2026-09-12 17:41:51 +0900

    Add files

---
+++ src/main/java/com/seoulit/it_manhunt/ItManhuntApplication.java
@@ -0,0 +1,14 @@
+package com.seoulit.it_manhunt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItManhuntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItManhuntApplication.class, args);
+    }
+
+}
+pw
